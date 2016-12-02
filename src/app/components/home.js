240aa133b8078527bd7dc2382c758aa28c10e95a
Higino <higino@gmail.com>
2016-12-02 15:17:15 +0000

    Adding state to Home component

--- src/app/components/home.js
+++ src/app/components/home.js
@@ -1,19 +1,33 @@
 import React from "react";
 
 export class Home extends React.Component {
+    constructor (props) {
+        super();
+        this.state = {
+            // We should not assign props to a state unless it is an initialization value, which is this case.
+            age: props.initialAge,
+            status: 'Single' // A dummy state property to demonstrate that the setState function does not change its value
+        };
+        setTimeout(() => {
+            this.setState({
+                status: 'Married'
+            })
+        }, 3000)
+    }
+
+    onMakeOlder() {
+        this.setState({
+            age: this.state.age + 3
+        });
+    }
+
     render() {
-        console.log(this.props);
         return (
             <div>
                 <p>In a new Component</p>
-                <p>Your name is {this.props.name}, your age is {this.props.age}</p>
-                <p>User object => Name: {this.props.user.name}</p>
-                    <div>
-                        <h4>Hobbies</h4>
-                        <ul>
-                            {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-                        </ul>
-                    </div>
+                <p>Your name is {this.props.name}, your age is {this.state.age}</p>
+                <p>Your status is {this.props.status}, your age is {this.state.status}</p>
+                <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me Older</button>
             </div>
         );
     }
